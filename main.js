fd26ea5773ebd9b2dc9d1664b4fbfbd104fbdcfa
mjpolak <marcin.jakub.polak@gmail.com>
2017-01-22 12:06:23 +0100

    validations of connections

--- main.js
+++ main.js
@@ -1,72 +1,91 @@
-
 angular.module('todoApp', ['ui.bootstrap', 'ui.layout'])
-  .controller('BodyController', function ($scope) {
+    .controller('BodyController', function($scope) {
 
-      // Checks if the browser is supported
-      if (!mxClient.isBrowserSupported()) {
-          // Displays an error message if the browser is not supported.
-          mxUtils.error('Browser is not supported!', 200, false);
-      }
-      else {
+        // Checks if the browser is supported
+        if (!mxClient.isBrowserSupported()) {
+            // Displays an error message if the browser is not supported.
+            mxUtils.error('Browser is not supported!', 200, false);
+        } else {
 
-          // Creates the graph inside the given container
-          
+            // Creates the graph inside the given container
 
-          // Enables rubberband selection
-          //new mxRubberband(graph);
 
-          // Gets the default parent for inserting new cells. This
-          // is normally the first child of the root (ie. layer 0).
-          var parent = graph.getDefaultParent();
+            // Enables rubberband selection
+            //new mxRubberband(graph);
 
-          // Settings for edges
+            // Gets the default parent for inserting new cells. This
+            // is normally the first child of the root (ie. layer 0).
+            var parent = graph.getDefaultParent();
 
-          // Adds cells to the model in a single step
+            // Settings for edges
 
-          var edgeStyle = graph.getStylesheet().getDefaultEdgeStyle();
+            // Adds cells to the model in a single step
 
-        var keyHandler = new mxKeyHandler(graph);
-        keyHandler.bindKey(46, function(evt)
-        {
-            console.log(graph.cellsEditable);
-        });
-          //edgeStyle[mxConstants.STYLE_EDGE] = 'Loop';
-          //edgeStyle[mxConstants.STYLE_CURVED] = '1';
-          
+            var edgeStyle = graph.getStylesheet().getDefaultEdgeStyle();
 
-        var test = {
-            toString: function(){
-                return "work in progress";
+            var keyHandler = new mxKeyHandler(graph);
+            keyHandler.bindKey(46, function(evt) {
+                //console.log(graph.cellsEditable);
+                if (graph.isEnabled()) {
+                    graph.removeCells();
+                }
+            });
+
+            keyHandler.bindKey(79, function(evt) {
+                console.log(graph.getEdgeValidationError)
+            });
+            //edgeStyle[mxConstants.STYLE_EDGE] = 'Loop';
+            //edgeStyle[mxConstants.STYLE_CURVED] = '1';
+
+
+            var start = {
+                source: true,
+                target: false,
+                toString: function() {
+                    return "";
+                }
             }
+
+            var koniec = {
+                source: false,
+                target: true,
+                toString: function() {
+                    return "";
+                }
+            }
+
+            var obiekt = {
+                source: true,
+                target: true,
+                toString: function() {
+                    return "Obiekt";
+                }
+            }
+
+
+            graph.getModel().beginUpdate();
+            try {
+
+                var v1 = graph.insertVertex(parent, null, start, 200, 20, 30, 30, 'start');
+                var v2 = graph.insertVertex(parent, null, koniec, 200, 200, 30, 30, 'end');
+                var v3 = graph.insertVertex(parent, null, obiekt, 200, 100, 100, 50);
+                //   var v2 = graph.insertVertex(parent, null, 'World!', 200, 150, 80, 30);
+                //   var e1 = graph.insertEdge(parent, null, '', v1, v2);
+                //   var e2 = graph.insertEdge(parent, null, '', v1, v1, mxConstants.STYLE_CURVED+'=1;'+mxConstants.STYLE_EDGE+'=Loop');
+            } finally {
+                // Updates the display
+                graph.getModel().endUpdate();
+            }
+            var toolbox = document.getElementById('toolbox');
+
+
+            wnd = new mxWindow('Test', toolbox, (0.8 * container.clientWidth) - 200, 50, 200, null, true, true);
+            wnd.setMaximizable(false);
+            wnd.setScrollable(false);
+            wnd.setResizable(false);
+            wnd.setMinimizable(false);
+            wnd.setVisible(true);
+
+            console.log(v1);
         }
-
-
-          graph.getModel().beginUpdate();
-          try {
-
-              var v1 = graph.insertVertex(parent, null, test, 200, 20, 30, 30,'start');
-              var v2 = graph.insertVertex(parent, null, test, 200, 200, 30, 30,'end');
-
-              var v3 = graph.insertVertex(parent, null, 'Any', 200, 100, 100, 50);
-            //   var v2 = graph.insertVertex(parent, null, 'World!', 200, 150, 80, 30);
-            //   var e1 = graph.insertEdge(parent, null, '', v1, v2);
-            //   var e2 = graph.insertEdge(parent, null, '', v1, v1, mxConstants.STYLE_CURVED+'=1;'+mxConstants.STYLE_EDGE+'=Loop');
-          }
-          finally {
-              // Updates the display
-              graph.getModel().endUpdate();
-          }
-          var toolbox = document.getElementById('toolbox');
-
-          
-          wnd = new mxWindow('Test', toolbox, (0.8*container.clientWidth) - 200, 50, 200, null, true, true);
-          wnd.setMaximizable(false);
-          wnd.setScrollable(false);
-          wnd.setResizable(false);
-          wnd.setMinimizable(false);
-          wnd.setVisible(true);
-
-          console.log(v1);
-      }
-  });
-
+    });
--- src/init_graph.js
+++ src/init_graph.js
@@ -2,11 +2,47 @@ var container = document.getElementById('graph_container');
 var graph = new mxGraph(container);
 
 graph.setCellsEditable(false);
+graph.setConnectable(true);
+graph.setCellsResizable(false);
+graph.setAllowLoops(true);
+new mxRubberband(graph);
 
-graph.setCellsResizable( false);
+mxGraph.prototype.isValidEnding = function(cell) {
+    return (cell == null && this.allowDanglingEdges) ||
+        (cell != null && (!this.model.isEdge(cell) ||
+            this.connectableEdges) && this.isCellConnectable(cell));
+}
 
-function StartShape()
-{
+mxGraph.prototype.isValidSource = function(cell) {
+    if (cell != null && cell.value && cell.value.source == false)
+        return false;
+    return this.isValidEnding(cell);
+}
+
+mxGraph.prototype.isValidTarget = function(cell) {
+    if (cell != null && cell.value && cell.value.target == false)
+        return false;
+    return this.isValidEnding(cell);
+}
+
+graph.validateEdge = function(edge, source, target) {
+
+    if (source.value != null && source.value.source == false)
+        return 'Cannot be source';
+    if (target.value != null && target.value.target == false)
+        return 'Cannot be target';
+
+    for (var i in source.edges) {
+        if (source.edges[i].target === target) {
+            return 'Only single connection';
+        }
+    }
+
+    return null;
+}
+
+
+function StartShape() {
     mxDoubleEllipse.call(this);
 }
 
@@ -14,14 +50,13 @@ mxUtils.extend(StartShape, mxDoubleEllipse);
 mxCellRenderer.prototype.defaultShapes['shape_start'] = StartShape;
 var startStyle = new Object();
 startStyle[mxConstants.STYLE_SHAPE] = 'shape_start';
-startStyle[mxConstants.STYLE_FILLCOLOR] ='#FFFFFF';
-startStyle[mxConstants.STYLE_STROKECOLOR] ='#000000';
-startStyle[mxConstants.STYLE_STROKEWIDTH] =2;
+startStyle[mxConstants.STYLE_FILLCOLOR] = '#FFFFFF';
+startStyle[mxConstants.STYLE_STROKECOLOR] = '#000000';
+startStyle[mxConstants.STYLE_STROKEWIDTH] = 2;
 
 graph.getStylesheet().putCellStyle('start', startStyle);
 
-function EndShape()
-{
+function EndShape() {
     mxEllipse.call(this);
 }
 
@@ -29,9 +64,9 @@ mxUtils.extend(EndShape, mxEllipse);
 mxCellRenderer.prototype.defaultShapes['shape_end'] = EndShape;
 var endStyle = new Object();
 endStyle[mxConstants.STYLE_SHAPE] = 'shape_end';
-endStyle[mxConstants.STYLE_FILLCOLOR] ='#FFFFFF';
-endStyle[mxConstants.STYLE_STROKECOLOR] ='#000000';
-endStyle[mxConstants.STYLE_STROKEWIDTH] =4;
+endStyle[mxConstants.STYLE_FILLCOLOR] = '#FFFFFF';
+endStyle[mxConstants.STYLE_STROKECOLOR] = '#000000';
+endStyle[mxConstants.STYLE_STROKEWIDTH] = 4;
 
 graph.getStylesheet().putCellStyle('end', endStyle);
 
@@ -43,5 +78,5 @@ defaultStyle[mxConstants.STYLE_ROUNDED] = true;
 defaultStyle[mxConstants.STYLE_SHADOW] = true;
 defaultStyle[mxConstants.STYLE_ARCSIZE] = 20;
 defaultStyle[mxConstants.STYLE_FILLCOLOR] = '#FFFFFF';
-defaultStyle[mxConstants.STYLE_STROKECOLOR] ='#000000';
-defaultStyle[mxConstants.STYLE_FONTCOLOR] ='#000000';
+defaultStyle[mxConstants.STYLE_STROKECOLOR] = '#000000';
+defaultStyle[mxConstants.STYLE_FONTCOLOR] = '#000000';
